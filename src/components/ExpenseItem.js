7936fa8bc960135d461e1fc8de6c018b654d0a48
ritik sharma <bharadwajritik1@gmail.com>
2023-01-04 02:12:20 +0530

    usingProp_viaLoops

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,21 @@
-function ExpenseItem(){
+import './ExpenseItem.css'
 
-    return (
-<>
-        <h2>Expense Item!</h2>
-        <h2>Food Rs 10</h2>
-        <h2>Petrol Rs 100</h2>
-        <h2>Movies Rs 200</h2>
-    </>
-    );
+function ExpenseItem(props) {
+   const month=props.date.toLocaleString('en-US',{month:'long'})
+   const day=props.date.toLocaleString('en-US',{day:'2-digit'})
+  const year=props.date.getFullYear()
+   return (
+    <div className='expense-item'>
+      <div>{month}</div>
+      <div>{day}</div>
+      <div>{year}</div>
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+        <div>{props.location}</div>
+      </div>
+    </div>
+  );
 }
 
-export default ExpenseItem
+export default ExpenseItem;
