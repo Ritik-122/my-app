92c0c63c49b76d2618769083aa7af5d217752fbe
ritik sharma <bharadwajritik1@gmail.com>
2023-01-18 23:31:13 +0530

    adding button to update dollar

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,13 +2,31 @@ import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate.js';
 import ExpenseDetails from './ExpenseDetails.js';
 import Card from '../UI/Card.js'
+import React,{useState} from 'react'
 
 const ExpenseItem=(props)=> {
+
+  const [title,setTitle] = useState(props.title)
+  const [exp,setExp]= useState(props.amount)
+
+  
+
+  const clickHandler=()=>{
+    setTitle('Updated!')
+    
+  }
+  const clickHandler2=()=>{
+     
+    setExp('$100')
+
+  }
    
    return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
-      <ExpenseDetails title={props.title} amount={props.amount} />
+      <ExpenseDetails title={title} amount={exp} />
+      <button onClick={clickHandler}>Change</button>
+      <button onClick={clickHandler2}>Expens</button>
       </Card>
   );
 }
